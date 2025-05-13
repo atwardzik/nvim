be8b7072b0760447146a1650cd0866f4e27077fb
Artur Twardzik <polym.t@protonmail.com>
2025-05-13 15:03:56 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,50 @@
+-- disable netrw at the very start of your init.lua - for nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("config.lazy")
+
+require("nvim-tree").setup()
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+
+
+
+
+-- init vim
+vim.wo.number = true            -- lines numbering
+vim.opt.wrap = false            -- disable wrapping lines
+vim.opt.clipboard="unnamedplus" -- enable clipboard for macos
+vim.opt.tabstop = 8             -- set tab to be 8 spaces
+vim.opt.expandtab = true        -- 
+
+
+
+
+-- setup go to definition to use LSP
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+
+
+-- setup telescope
+local builtin = require('telescope.builtin')
+
+vim.keymap.set("n", "<leader>ld", function()
+        builtin.diagnostics(require("telescope.themes").get_dropdown({
+                previewer = true,
+                wrap_results = true,
+                line_width = 'full',
+        }))
+        end,
+        { desc = "Telescope Diagnostics (Dropdown)" }
+)
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+
---
+++ lua/plugins/spec_lsp_manager.lua
@@ -0,0 +1,8 @@
+return {
+        'mason-org/mason.nvim',
+        'mason-org/mason-lspconfig.nvim',
+        'neovim/nvim-lspconfig',
+        config = function()
+                require('mason').setup()
+        end,
+}
